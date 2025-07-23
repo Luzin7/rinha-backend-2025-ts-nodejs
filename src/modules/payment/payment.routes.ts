a524f392f9c2b6cb7b5a45204b1cb84f94aa20e6
Luan Victor <lvictor.dutra@gmail.com>
2025-07-23 01:01:52 -0300

    feat: add controllers to route

--- src/modules/payment/payment.routes.ts
+++ src/modules/payment/payment.routes.ts
@@ -1,13 +1,27 @@
 import { FastifyInstance } from 'fastify';
 import { redis } from '../../infra/cache/redis';
-import { PaymentController } from './payment.controllers';
-import { PaymentService } from './payment.service';
+import { PaymentSummaryController } from './controllers/PaymentSummary.controller';
+import { ProcessPaymentController } from './controllers/ProcessPayment.controller';
+import { PurgePaymentsController } from './controllers/PurgePayments.controller';
+import { PaymentSummaryService } from './services/PaymentSummary.service';
+import { ProcessPaymentService } from './services/ProcessPayment.service';
+import { PurgePaymentsService } from './services/PurgePayments.service';
 
 export async function paymentRoutes(app: FastifyInstance) {
-  const paymentsService = new PaymentService(redis);
-  const paymentController = new PaymentController(paymentsService);
+  const processPaymentService = new ProcessPaymentService(redis);
+  const processPaymentController = new ProcessPaymentController(
+    processPaymentService,
+  );
+  const paymentSummaryService = new PaymentSummaryService(redis);
+  const paymentSummaryController = new PaymentSummaryController(
+    paymentSummaryService,
+  );
+  const purgePaymentsService = new PurgePaymentsService(redis);
+  const purgePaymentsController = new PurgePaymentsController(
+    purgePaymentsService,
+  );
 
-  app.post('/payments', paymentController.create);
-  app.get('/payments-summary', paymentController.getSummary);
-  app.post('/purge-payments', paymentController.purge);
+  app.post('/payments', processPaymentController.handle);
+  app.get('/payments-summary', paymentSummaryController.handle);
+  app.post('/purge-payments', purgePaymentsController.handle);
 }

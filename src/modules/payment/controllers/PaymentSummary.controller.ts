fbfcb0b58af13fae267ce195f2f6cca849bdf60a
Luan Victor <lvictor.dutra@gmail.com>
2025-07-23 01:02:28 -0300

    feat: create payment summary controller

---
+++ src/modules/payment/controllers/PaymentSummary.controller.ts
@@ -0,0 +1,36 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import z from 'zod';
+import { Controller } from '../../../shared/contracts/Controller';
+import { summaryQuerySchema } from '../schemas/summaryQueue.schema';
+import { PaymentSummaryService } from '../services/PaymentSummary.service';
+
+export class PaymentSummaryController implements Controller {
+  constructor(private paymentService: PaymentSummaryService) {}
+
+  async handle(request: FastifyRequest, reply: FastifyReply): Promise<void> {
+    const validation = summaryQuerySchema.safeParse(request.query);
+
+    if (!validation.success) {
+      return reply.status(400).send({
+        error: 'Invalid query parameters',
+        issues: z.treeifyError(validation.error),
+      });
+    }
+
+    const { from, to } = validation.data;
+
+    const response = await this.paymentService.execute({
+      from,
+      to,
+    });
+
+    if (response.isLeft()) {
+      const error = response.value;
+      return reply.status(error.statusCode).send({ message: error.message });
+    }
+
+    const summary = response.value;
+
+    reply.send(summary);
+  }
+}

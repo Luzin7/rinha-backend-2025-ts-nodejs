d64a3541a96ce2cdfc62c3c2647aeb6230421dbc
Luan Victor <lvictor.dutra@gmail.com>
2025-07-23 01:02:51 -0300

    feat: create purge payment to test  controller

---
+++ src/modules/payment/controllers/PurgePayments.controller.ts
@@ -0,0 +1,18 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { Controller } from '../../../shared/contracts/Controller';
+import { PurgePaymentsService } from '../services/PurgePayments.service';
+
+export class PurgePaymentsController implements Controller {
+  constructor(private paymentService: PurgePaymentsService) {}
+
+  async handle(_: FastifyRequest, reply: FastifyReply): Promise<void> {
+    const result = await this.paymentService.execute();
+
+    if (result.isLeft()) {
+      const error = result.value;
+      return reply.status(error.statusCode).send({ error: error.message });
+    }
+
+    return reply.status(204).send();
+  }
+}

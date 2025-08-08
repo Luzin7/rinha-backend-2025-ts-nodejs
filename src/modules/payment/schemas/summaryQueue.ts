74fe230aa95202caff99e159a1090876ea8b722e
Luan Victor <lvictor.dutra@gmail.com>
2025-08-08 18:43:24 -0300

    feat: make 'from' and 'to' properties optional in summaryQuerySchema

--- src/modules/payment/schemas/summaryQueue.ts
+++ src/modules/payment/schemas/summaryQueue.ts
@@ -1,6 +1,6 @@
 import z from 'zod';
 
 export const summaryQuerySchema = z.object({
-  from: z.iso.datetime(),
-  to: z.iso.datetime(),
+  from: z.iso.datetime().optional(),
+  to: z.iso.datetime().optional(),
 });
